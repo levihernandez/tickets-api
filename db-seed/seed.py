43216e1feb061f52d4d7092224e1bb8072f0d974
Julian Levi Hernandez <35246411+levihernandez@users.noreply.github.com>
2023-11-09 14:44:21 -0500

    Update seed.py

--- db-seed/seed.py
+++ db-seed/seed.py
@@ -1,17 +1,20 @@
 from sqlalchemy import create_engine, Column, String, ForeignKey, Numeric, DECIMAL
-from sqlalchemy.pool import QueuePool 
+from sqlalchemy.pool import QueuePool
 from sqlalchemy.orm import declarative_base, sessionmaker
 import uuid
 from faker import Faker
 import random
 from tqdm import tqdm
 import argparse
+import json
 
 fake = Faker()
 Base = declarative_base()
 
-pool_size = 20
-max_overflow = 25
+
+# pool_size = 20
+# max_overflow = 25
+# No need to define pool_size and max_overflow here, they will be taken from the config
 
 class User(Base):
     __tablename__ = 'users'
@@ -43,6 +46,25 @@ class Payment(Base):
     status = Column(String)
 
 
+def create_database_engine(db_config, ssl_config, secure):
+    if secure:
+        ssl_args = f"?sslmode=verify-full&sslrootcert={ssl_config['rootcert']}&sslcert={ssl_config['certfile']}&sslkey={ssl_config['keyfile']}"
+    else:
+        ssl_args = "?sslmode=disable"
+
+    connection_string = f"cockroachdb://{db_config['user']}:{db_config['password']}@" \
+                        f"{db_config['host']}:{db_config['port']}/{db_config['database']}{ssl_args}"
+
+    engine = create_engine(
+        connection_string,
+        poolclass=QueuePool,
+        pool_size=db_config['pool_size'],
+        max_overflow=db_config['pool_max_conns'] - db_config['pool_size'],
+        pool_pre_ping=True
+    )
+    return engine
+
+
 def create_fake_data(session, num_users, num_purchases, num_events, num_cancellations, num_payments, batch_size=100):
     user_ids = []
     event_ids = []
@@ -55,7 +77,8 @@ def create_fake_data(session, num_users, num_purchases, num_events, num_cancella
     session.commit()
 
     for _ in tqdm(range(num_events), desc='Creating Events'):
-        event = Event(name=fake.sentence(nb_words=4), type=random.choice(['concert', 'opera', 'theater','football','soccer','baseball','basketball','conference']),
+        event = Event(name=fake.sentence(nb_words=4), type=random.choice(
+            ['concert', 'opera', 'theater', 'football', 'soccer', 'baseball', 'basketball', 'conference']),
                       status='scheduled')
         event_ids.append(event)
     session.add_all(event_ids)
@@ -88,33 +111,30 @@ def create_fake_data(session, num_users, num_purchases, num_events, num_cancella
 
 
 def main(args):
-    # Insecure connection
-    #  engine = create_engine('cockroachdb://{user}@192.168.86.74:26257/tickets')
-    # Secure connection
-    #  engine = create_engine(
-    #    'cockroachdb://{user}:{password}@{crdb-url}:26257/tickets?sslmode=verify-full&sslrootcert={home_certs}/certs/ca.crt&sslcert={home_certs}/certs/client.julian.crt&sslkey={home_certs}/certs/client.julian.key'
-    # )
-    engine = create_engine(
-        'cockroachdb://{user}:{password}@{crdb-url}:26257/tickets?sslmode=verify-full&sslrootcert={home_certs}/certs/ca.crt&sslcert={home_certs}/certs/client.julian.crt&sslkey={home_certs}/certs/client.julian.key',
-        poolclass=QueuePool,
-        pool_size=pool_size,
-        max_overflow=max_overflow,
-        pool_pre_ping=True
-    )
+    with open(args.config) as f:
+        config = json.load(f)
+    print(config)
+    db_config = config['cockroach']['database']
+    ssl_config = config['cockroach']['ssl']
+    print(db_config)
+    engine = create_database_engine(db_config, ssl_config, args.secure)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    create_fake_data(session, args.num_users, args.num_purchases, args.num_events, args.num_cancellations, args.num_payments)
+    create_fake_data(session, args.num_users, args.num_purchases, args.num_events, args.num_cancellations,
+                     args.num_payments)
 
     session.close()
     print("Data generation complete!")
 
 
 if __name__ == "__main__":
-    # python seed.py --num_users 1000 --num_purchases 15000000 --num_events 200000 --num_cancellations 200000 --num_payments 20000
+    # python db-seed/seed.py (--secure or null) --config config/config.json --num_users 1 --num_purchases 1 --num_events 1 --num_cancellations 1 --num_payments 1
     parser = argparse.ArgumentParser(description='Generate fake data for ticket purchasing service.')
+    parser.add_argument('--secure', action='store_true', help='Use secure database connection')
+    parser.add_argument('--config', type=str, default='config/config.json', help='Path to configuration file')
     parser.add_argument('--num_users', type=int, default=1000, help='Number of users to generate')
     parser.add_argument('--num_purchases', type=int, default=5000, help='Number of purchases to generate')
     parser.add_argument('--num_events', type=int, default=5000, help='Number of purchases to generate')
@@ -122,4 +142,5 @@ if __name__ == "__main__":
     parser.add_argument('--num_payments', type=int, default=5000, help='Number of payments to generate')
 
     args = parser.parse_args()
+    print(args)
     main(args)
